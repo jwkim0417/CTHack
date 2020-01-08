ec03cead616bb4cc5f46274a820f2b88a6e168d5
BlueHorn07 <51858327+BlueHorn07@users.noreply.github.com>
2020-01-09 02:07:05 +0900

    ResultBoard image insertion fail...

--- happy_ct/src/main/java/com/CTGUI/ResultBoard.java
+++ happy_ct/src/main/java/com/CTGUI/ResultBoard.java
@@ -13,6 +13,13 @@ import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import javax.swing.table.*;
 
+/* insert Image */
+import java.awt.*;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import javax.swing.ImageIcon;
+
 
 /**
 
@@ -25,16 +32,38 @@ public class ResultBoard extends JPanel {
     JTable Board;
     JScrollPane jscroll;
     
-    String[] headings = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-    String[][] data = {
-        {"Player 1", "", "", "", "", "", "", "", "", "", ""}, 
-        {"Boss", "", "", "", "", "", "", "", "", "", ""}, 
-    };
-    
     /* constructor */
     public ResultBoard(){
-        this.Board = new JTable(data, headings);
-        
+        Image image;
+        try{
+            // File path Problem??
+            // ref1: https://pkss.tistory.com/entry/JTable%EC%97%90-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EB%84%A3%EA%B8%B0
+            // ref2: https://stackoverflow.com/questions/25378231/cant-add-image-using-imageicon-to-jtable-cell
+            image = ImageIO.read(new File("src/main/java/images/bg.jpg"));
+        } catch(IOException e){
+            return;
+        }
+        ImageIcon test = new ImageIcon(image);
+
+        String[] headings = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        Object[][] data = {
+            {"Player 1", test, "", "", "", "", "", "", "", "", ""}, 
+            {"Boss", "", "", "", "", "", "", "", "", "", ""}, 
+        };
+        DefaultTableModel model = new DefaultTableModel(data, headings)
+        {
+            @Override
+            public Class getColumnClass(int column){
+                switch(column){
+                    case 0:
+                    case 1: return Integer.class;
+                    case 2: return ImageIcon.class;
+                    default: return Object.class;
+                }
+            }
+        };
+        this.Board = new JTable(model);
+
         /* JTable header & entry center-alignment */
         DefaultTableCellRenderer celAlignCenter = new DefaultTableCellRenderer();
         celAlignCenter.setHorizontalAlignment(SwingConstants.CENTER);
@@ -57,6 +86,4 @@ public class ResultBoard extends JPanel {
         this.Board.setValueAt(result1, 0, Turn);
         this.Board.setValueAt(result2, 1, Turn);
     }
-    
-    
 }
